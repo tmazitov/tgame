8ef7bf1c758957bbcf93bb6b413a1d94cdb1e682
Timur Mazitov <tlenovo@lenovo>
2024-01-27 01:48:00 +0400

    add player and idle animation

--- cmd/main.go
+++ cmd/main.go
@@ -23,6 +23,7 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/images"
 	"github.com/tmazitov/tgame.git/internal/ground"
+	"github.com/tmazitov/tgame.git/internal/player"
 	gm_layer "github.com/tmazitov/tgame.git/pkg/gm_layer"
 	gm_machine "github.com/tmazitov/tgame.git/pkg/gm_machine"
 	gm_obj "github.com/tmazitov/tgame.git/pkg/gm_obj"
@@ -68,5 +69,10 @@ func main() {
 		},
 	))
 
+	var player *player.Player = player.NewPlayer(0, 0, "../assets/textures/player.png")
+	if player == nil {
+		log.Fatal("Player is nil")
+	}
+	game.AddEntity(player)
 	game.Run()
 }
