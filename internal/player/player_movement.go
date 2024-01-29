c7d04f98a18fbde27eae5a36b654930da6b3efdd
Timur Mazitov <tlenovo@lenovo>
2024-01-29 15:25:19 +0400

    add the case handle when A and D or W and S is pressed

--- internal/player/player_movement.go
+++ internal/player/player_movement.go
@@ -16,30 +16,56 @@ const (
 
 func (p *Player) MovementHandler(keys []ebiten.Key) {
 
-	var pressedKeyFound bool = false
+	var (
+		pressedKeyFound bool   = false
+		pressedKeyArray []bool = []bool{false, false, false, false}
+	)
 
 	// TODO : bitmap to handle case then user press D and A
 
 	for _, key := range keys {
 		if key == ebiten.KeyW {
-			p.moveTop()
-			pressedKeyFound = true
-		}
-		if key == ebiten.KeyS {
-			p.moveBot()
-			pressedKeyFound = true
-		}
-		if key == ebiten.KeyD {
-			p.moveRight()
-			pressedKeyFound = true
+			pressedKeyArray[0] = true
 		}
 		if key == ebiten.KeyA {
-			p.moveLeft()
-			pressedKeyFound = true
+			pressedKeyArray[1] = true
+		}
+		if key == ebiten.KeyS {
+			pressedKeyArray[2] = true
+		}
+		if key == ebiten.KeyD {
+			pressedKeyArray[3] = true
 		}
 	}
 
-	if !pressedKeyFound && p.actionState != Idle_PlayerAction {
+	if pressedKeyArray[0] && pressedKeyArray[2] {
+		pressedKeyArray[0] = false
+		pressedKeyArray[2] = false
+	}
+
+	if pressedKeyArray[1] && pressedKeyArray[3] {
+		pressedKeyArray[1] = false
+		pressedKeyArray[3] = false
+	}
+
+	for _, key := range pressedKeyArray {
+		pressedKeyFound = pressedKeyFound || key
+	}
+
+	if pressedKeyFound {
+		if pressedKeyArray[0] {
+			p.moveTop()
+		}
+		if pressedKeyArray[1] {
+			p.moveLeft()
+		}
+		if pressedKeyArray[2] {
+			p.moveBot()
+		}
+		if pressedKeyArray[3] {
+			p.moveRight()
+		}
+	} else if p.actionState != Idle_PlayerAction {
 		p.lastAction = p.actionState
 		p.actionState = Idle_PlayerAction
 	}
